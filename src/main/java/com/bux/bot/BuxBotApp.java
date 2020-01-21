e212ad53bec5873f08b0d3b4aad5c7cb94aad8f9
Vasyl Kudriashov <vasyl.kudriashov@stylight.com>
2020-01-21 20:22:50 +0100

    wip

--- src/main/java/com/bux/bot/BuxBotApp.java
+++ src/main/java/com/bux/bot/BuxBotApp.java
@@ -4,7 +4,7 @@ import com.bux.bot.model.shared.TradingData;
 import com.bux.bot.service.data.TradingDataReader;
 import com.bux.bot.service.pricefeed.FeedService;
 
-public class TradingBotApp {
+public class BuxBotApp {
     public static void main(String[] args) {
         TradingData tradingData = TradingDataReader.readFromConsole();
         new FeedService().start(tradingData);
